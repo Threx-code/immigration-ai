5a5215be4d9977c161896d3c073c0add5cb9d95f
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-16 17:35:10 +0000

    gettting the system to run

--- src/users_access/apps.py
+++ src/users_access/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class PasswordResetConfig(AppConfig):
+class UsersAccessConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users_access"
 
