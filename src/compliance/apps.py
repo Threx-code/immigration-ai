c9bbbe9295e82e633d27426466f0f04c9b06b45f
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-16 17:35:07 +0000

    gettting the system to run

--- src/compliance/apps.py
+++ src/compliance/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.core.signals import request_started
 
 
-class PasswordResetConfig(AppConfig):
+class ComplianceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "ai_decisions"
+    name = "compliance"
+
+
+    def ready(self):
+        from .handlers.logging_setup import LoggingSetup
+        request_started.connect(LoggingSetup.safe_setup, dispatch_uid="audit-logs-init")
+
