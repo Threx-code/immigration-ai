05deb66784c627702b6925e2e1c6c0dca08a2261
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-16 17:35:11 +0000

    gettting the system to run

--- src/users_access/services/services.py
+++
@@ -1,9 +0,0 @@
-from src.ai_decisions.models.models import PasswordReset
-
-class PasswordResetService:
-    def __init__(self):
-        self.manager = PasswordReset.objects
-
-    def create(self, user):
-        created = self.manager.create_password_reset(user=user)
-
