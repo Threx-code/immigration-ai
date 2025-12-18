260c576f3be04c0df02547ac0bc03704db48b0a9
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-18 21:09:08 +0000

    user access, countries, states profiles doen

--- src/users_access/repositories/user_repository.py
+++ src/users_access/repositories/user_repository.py
@@ -1,89 +1,47 @@
-from typing import Optional
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import BaseUserManager
 from django.db import transaction
 from users_access.models.user import User
 from helpers import fields as input_fields
-import logging
-
-logger = logging.getLogger('django')
 
 
 class UserRepository:
 
     @staticmethod
-    def create_user(email, password, first_name, last_name):
+    def create_user(email, password):
+        """
+        Create a new user with profile.
+        PII fields (first_name, last_name) are stored in UserProfile.
+        """
         with transaction.atomic():
             normalized_email = BaseUserManager.normalize_email(email)
-            user = User.objects.create(
-                email=normalized_email,
-                first_name=first_name,
-                last_name=last_name,
-                role=input_fields.USER
-            )
+            user = User.objects.create(email=normalized_email, role=input_fields.USER)
 
             user.set_password(password)
             user.is_active = True
             user.is_staff = False
             user.is_superuser = False
             user.save(using=User.objects._db)
+            
             user.full_clean()
             return user
 
     @staticmethod
-    def create_superuser(email, password, first_name, last_name):
-        user = UserRepository.create_user(email, password, first_name, last_name)
+    def create_superuser(email, password):
+        user = UserRepository.create_user(email, password)
         user.role = input_fields.ADMIN
         user.is_superuser = True
         user.is_staff = True
+
         user.full_clean()
         user.save()
         return user
 
     @staticmethod
-    def update_user(user, **fields):
-        with transaction.atomic():
-            for key, value in fields.items():
-                setattr(user, key, value)
-            user.save(using=User.objects._db)
-            user.full_clean()
-            return user
-
-    @staticmethod
-    def update_avatar(user, avatar):
-        with transaction.atomic():
-            old_avatar = user.avatar
-            user.avatar = avatar
-            user.full_clean()
-            user.save()
-            if old_avatar and old_avatar.name and old_avatar.name != user.avatar.name:
-                old_avatar.delete(save=False)
-            return user
-
-    @staticmethod
-    def remove_avatar(user):
-        with transaction.atomic():
-            old_avatar = user.avatar
-            user.avatar = None
-            user.full_clean()
-            user.save()
-            if old_avatar and old_avatar.name:
-                old_avatar.delete(save=False)
-            return user
-
-    @staticmethod
-    def update_names(user, first_name: Optional[str], last_name: Optional[str]):
-        with transaction.atomic():
-            user.first_name = first_name if first_name else user.first_name
-            user.last_name = last_name if last_name else user.last_name
-            user.full_clean()
-            user.save()
-            return user
-
-    @staticmethod
     def activate_user(user):
         with transaction.atomic():
             user.is_verified = True
+
             user.full_clean()
             user.save()
             return user
@@ -92,6 +50,7 @@ class UserRepository:
     def update_password(user, password):
         with transaction.atomic():
             user.password = make_password(password)
+
             user.full_clean()
             user.save()
             return user
@@ -100,6 +59,17 @@ class UserRepository:
     def is_verified(user):
         with transaction.atomic():
             user.is_verified = True
+            user.full_clean()
+            user.save()
+            return user
+
+
+    @staticmethod
+    def update_last_assigned_at(user):
+        """Update last assigned time for reviewer assignment tracking."""
+        from django.utils import timezone
+        with transaction.atomic():
+            user.last_assigned_at = timezone.now()
             user.full_clean()
             user.save()
             return user
