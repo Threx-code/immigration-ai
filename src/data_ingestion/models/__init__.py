3064da03c50a599cdbf005aad93b08d400910495
Oluwatosin Amokeodo <oluwatosin@MacBook-Pro.local>
2025-12-22 22:00:22 +0000

    started ingestion system

---
+++ src/data_ingestion/models/__init__.py
@@ -0,0 +1,16 @@
+from .data_source import DataSource
+from .source_document import SourceDocument
+from .document_version import DocumentVersion
+from .document_diff import DocumentDiff
+from .parsed_rule import ParsedRule
+from .rule_validation_task import RuleValidationTask
+
+__all__ = [
+    'DataSource',
+    'SourceDocument',
+    'DocumentVersion',
+    'DocumentDiff',
+    'ParsedRule',
+    'RuleValidationTask',
+]
+
