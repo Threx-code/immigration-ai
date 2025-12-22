51b37be712c4a75f63806eab01a12a79fb908652
Oluwatosin Amokeodo <oluwatosin@MacBook-Pro.local>
2025-12-22 22:00:22 +0000

    started ingestion system

---
+++ src/data_ingestion/services/__init__.py
@@ -0,0 +1,10 @@
+from .ingestion_service import IngestionService
+from .data_source_service import DataSourceService
+from .rule_parsing_service import RuleParsingService
+
+__all__ = [
+    'IngestionService',
+    'DataSourceService',
+    'RuleParsingService',
+]
+
