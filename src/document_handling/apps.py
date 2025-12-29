794a719d511666942e8a992539ad8235b4fb3ae6
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:35:47 +0000

    adding signals and tasks

--- src/document_handling/apps.py
+++ src/document_handling/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DocumentHandlingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "document_handling"
+
+    def ready(self):
+        from .signals import document_signals
