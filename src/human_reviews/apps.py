07696316dad928f1f1a262da716ac52e105703df
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:35:47 +0000

    adding signals and tasks

--- src/human_reviews/apps.py
+++ src/human_reviews/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HumanReviewConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "human_reviews"
+
+    def ready(self):
+        from .signals import review_signals
