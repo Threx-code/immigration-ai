612b2d036092dea9d7ac3bff27fc58e37e82e198
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:15:37 +0000

    adding most of the models, repositories, selectors and services

--- src/data_ingestion/serializers/__init__.py
+++ src/data_ingestion/serializers/__init__.py
@@ -2,11 +2,50 @@ from .data_source.create import DataSourceCreateSerializer
 from .data_source.read import DataSourceSerializer, DataSourceListSerializer
 from .data_source.update_delete import DataSourceUpdateSerializer, DataSourceIngestionTriggerSerializer
 
+from .source_document.read import SourceDocumentSerializer, SourceDocumentListSerializer
+
+from .document_version.read import DocumentVersionSerializer, DocumentVersionListSerializer
+
+from .document_diff.read import DocumentDiffSerializer, DocumentDiffListSerializer
+
+from .parsed_rule.read import ParsedRuleSerializer, ParsedRuleListSerializer
+from .parsed_rule.update_delete import ParsedRuleUpdateSerializer, ParsedRuleStatusUpdateSerializer
+
+from .rule_validation_task.read import RuleValidationTaskSerializer, RuleValidationTaskListSerializer
+from .rule_validation_task.update_delete import (
+    RuleValidationTaskUpdateSerializer,
+    RuleValidationTaskAssignSerializer,
+    RuleValidationTaskApproveSerializer,
+    RuleValidationTaskRejectSerializer
+)
+
 __all__ = [
+    # Data Source
     'DataSourceCreateSerializer',
     'DataSourceSerializer',
     'DataSourceListSerializer',
     'DataSourceUpdateSerializer',
     'DataSourceIngestionTriggerSerializer',
+    # Source Document
+    'SourceDocumentSerializer',
+    'SourceDocumentListSerializer',
+    # Document Version
+    'DocumentVersionSerializer',
+    'DocumentVersionListSerializer',
+    # Document Diff
+    'DocumentDiffSerializer',
+    'DocumentDiffListSerializer',
+    # Parsed Rule
+    'ParsedRuleSerializer',
+    'ParsedRuleListSerializer',
+    'ParsedRuleUpdateSerializer',
+    'ParsedRuleStatusUpdateSerializer',
+    # Rule Validation Task
+    'RuleValidationTaskSerializer',
+    'RuleValidationTaskListSerializer',
+    'RuleValidationTaskUpdateSerializer',
+    'RuleValidationTaskAssignSerializer',
+    'RuleValidationTaskApproveSerializer',
+    'RuleValidationTaskRejectSerializer',
 ]
 
