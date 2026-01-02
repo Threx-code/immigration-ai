cdaa493374eb756c6102cfa5d4692e6b59e01284
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-02 21:21:38 +0000

    ocr implemented

--- src/document_handling/services/__init__.py
+++ src/document_handling/services/__init__.py
@@ -1,8 +1,14 @@
 from .case_document_service import CaseDocumentService
 from .document_check_service import DocumentCheckService
+from .ocr_service import OCRService
+from .document_classification_service import DocumentClassificationService
+from .file_storage_service import FileStorageService
 
 __all__ = [
     'CaseDocumentService',
     'DocumentCheckService',
+    'OCRService',
+    'DocumentClassificationService',
+    'FileStorageService',
 ]
 
