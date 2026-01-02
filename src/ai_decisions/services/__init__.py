0a6896cee8f1fd306680b4753cc5de59af65ab63
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-02 21:21:37 +0000

    ocr implemented

--- src/ai_decisions/services/__init__.py
+++ src/ai_decisions/services/__init__.py
@@ -1,10 +1,16 @@
 from .eligibility_result_service import EligibilityResultService
 from .ai_reasoning_log_service import AIReasoningLogService
 from .ai_citation_service import AICitationService
+from .vector_db_service import VectorDBService
+from .embedding_service import EmbeddingService
+from .ai_reasoning_service import AIReasoningService
 
 __all__ = [
     'EligibilityResultService',
     'AIReasoningLogService',
     'AICitationService',
+    'VectorDBService',
+    'EmbeddingService',
+    'AIReasoningService',
 ]
 
